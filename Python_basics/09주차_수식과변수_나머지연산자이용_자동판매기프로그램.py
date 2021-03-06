03df076080f5778022b8b038c9568e3480a9f513
Yebin Lee <71310074+YebinLeee@users.noreply.github.com>
2021-03-06 18:58:42 +0900

    rename

--- Python_basics/09주차_수식과변수_나머지연산자이용_자동판매기프로그램.py
+++ Python_basics/09주차_수식과변수_나머지연산자이용_자동판매기프로그램.py
@@ -3,8 +3,11 @@
 get_money = int(input("투입한 돈: "))
 price = int(input("물건값: "))
 remainder = get_money - price
+
 print("거스름돈:", remainder)
+
 coin500 = remainder // 500 # 500원의 동전은 500으로 나눈 몫
 coin100 = (remainder%500) // 100 # 100원의 동전은 500원으로 나눈 나머지를 100으로 나눈 몫
+
 print("500원 동전의 개수: ", coin500)
 print("100원 동전의 개수: ", coin100) 
