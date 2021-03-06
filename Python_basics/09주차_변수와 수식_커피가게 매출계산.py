859428fdccc9c390e8471458dd3c65c71399d0f3
Yebin Lee <71310074+YebinLeee@users.noreply.github.com>
2021-03-06 18:57:03 +0900

    rename

--- Python_basics/09주차_변수와 수식_커피가게 매출계산.py
+++ Python_basics/09주차_변수와 수식_커피가게 매출계산.py
@@ -3,7 +3,7 @@ caffelatte_price = 3300
 caffucino_price = 3500
 
 x = int(input("아메리카노 판매 개수: "))
-y = int(input("카푸라떼 판매 개수: "))
+y = int(input("카페라떼 판매 개수: "))
 z = int(input("카푸치노 판매 개수: "))
 sum = x*americano_price + y*caffelatte_price + z*caffucino_price
 
@@ -11,5 +11,5 @@ sales = americano_price*x
 sales = sales + caffelatte_price*y
 sales = sales + caffucino_price*z
 
-print(" 총 매출은", sum, "입니다.")
-print(" 총 매출은", sales, "입니다.")
+print("총 매출은", sum, "입니다.")
+print("총 매출은", sales, "입니다.")
